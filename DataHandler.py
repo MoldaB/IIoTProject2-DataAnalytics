d4c1483d7e0324cd2ce9558c2fabc778047698d4
Molda <hbeygel@gmail.com>
2018-07-08 13:00:21 +0300

    * created function for joining by column * created join function for production+defected * created join func for shifts+employees * created join func for production+sales ---------------------------------------

--- DataHandler.py
+++ DataHandler.py
@@ -42,6 +42,47 @@ class DataHandler:
         for filename in self.files:
             self.datasets[filename] = read_csv_from_path(path_for_csv_file(filename))
 
+    def join_production_sales(self):
+        result = self.join('Production','Sales')
+        return result
+
+    def join_production_defected(self):
+        result = self.join('Production', 'Defected_Parts', column = 'Box RFID')
+        return result
+
+    def join_shifts_employees(self):
+        result = self.join('Shifts','Employees', column = 'Emp ID')
+        return result
+        
+    def join(self, ds1_name, ds2_name, column = None):
+        try:
+            ds1 = self.datasets[ds1_name]
+            ds2 = self.datasets[ds2_name]
+            print("%s index is %s" % (ds1_name, ds1.index))
+            print("%s index is %s" % (ds2_name, ds2.index))
+            if column != None:
+                print("Selected Column Is %s" % (column))
+                # print(ds1[column].head())
+                # print(ds2[column].head())
+                result = pd.merge(ds1, ds2,how='inner', on=column)
+                return result
+            else:
+                return pd.concat([ds1,ds2], axis=1, join_axes=[ds1.index])
+        except Exception as e:
+            print(e)
+
+    def list_all_columns(self, selected_columns = []):
+        all_df_columns = {}
+        iterated_dfs = []
+        if not selected_columns:
+            iterated_dfs = self.files
+        else:
+            iterated_dfs = selected_columns
+        for df_name in iterated_dfs:
+            df = self.datasets[df_name]
+            all_df_columns[df_name] = list(df)
+        return all_df_columns
+
     def __str__(self):
         rep = ''
         for filename in self.files:
--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from DataHandler import DataHandler
 
 data_handler = DataHandler()
 
-print(data_handler)
+try:
+    print(data_handler)
+    list = data_handler.list_all_columns(selected_columns=['Failures','Employees','Shifts'])
+    print(list)
+
+    print(data_handler.join_shifts_employees().head())
+    # print(data_handler.join_production_defected().head())
+except Exception as e:
+    print(e)
